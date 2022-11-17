1800fb554585a57303b2878b211a691ea21981b2
Raquelpro <107865288+Raquelpro@users.noreply.github.com>
2022-11-17 11:30:02 -0600

    created apiurl

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,2 @@
 let Key = "cc0b84ae3c1b3dd45tb97od7d6ff24e5";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`;
-console.log(apiUrl);
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key=${key}&units=metric`;
